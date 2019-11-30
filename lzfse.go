b2497f3ca2421e8d4ecd6aed8c065434873abfc2
blacktop <blacktop@users.noreply.github.com>
2019-11-30 12:09:28 -0500

    Update lzfse.go

--- lzfse.go
+++ lzfse.go
@@ -6,7 +6,7 @@
 package lzfse
 
 /*
-#cgo windows LDFLAGS: -L${SRCDIR}\lzfse\build\Debug\ -llzfse
+#cgo windows LDFLAGS: -L${SRCDIR}/lzfse/build/Debug -llzfse.lib
 #cgo linux LDFLAGS: -L/usr/local/lib -llzfse
 #cgo darwin LDFLAGS: -L/usr/local/Cellar/lzfse/1.0/lib -llzfse
 #include "lzfse.h"
